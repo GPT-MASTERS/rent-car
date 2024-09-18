50b9f4b3274b4f05eb69ace175e6d1c247072dd9
Henrique Ferrario <henriqueplech1@gmail.com>
2024-09-18 00:02:03 -0300

    inset register page

--- app/Register/page.tsx
+++ app/Register/page.tsx
@@ -1,9 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="flex justify-center p-4">
+      <div className="flex flex-col">
+        <Card>
+          <CardTitle className="text-2xl flex justify-center items-center mt-4 ">
+            Crie sua conta
+          </CardTitle>
+          <CardContent className="flex justify-center ml-8 p-4">
+            <CardHeader>Nome</CardHeader>
+            <Input label="Name" className="mt-5 bg-slate-300" />
+            <CardHeader>Email</CardHeader>
+            <Input label="Name" className="mt-5 bg-slate-300" />
+            <Button className="bg-slate-600 ml-40 mr-4 mt-4">Cadastrar</Button>
+          </CardContent>
+          <CardFooter className="justify-center text-lg">Axium desenvolvimentos agradece.</CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
